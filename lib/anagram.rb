a65a4fdeb5296c1f0c4824a30d6110482f18c259
megann <megann.elizabeth.king@gmail.com>
2021-07-15 13:54:29 -0400

    commit tree ab6eb6df2c9e8dceac6b1aae8c21da5bb5c84cdc

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,22 @@
 # Your code goes here!
+class Anagram
+
+    attr_accessor :word
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(word_array)
+        matches = []
+        sorted_word = @word.chars.sort.join
+        word_array.each do |word|
+            sorted_word_array = word.chars.sort.join
+            if sorted_word_array == sorted_word
+                matches << word
+            end
+        end
+        matches
+    end
+
+end
